6ce0ff7f7bb76d4e4888447c71f5978cab1da70c
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-05-24 08:12:29 +0200

    fix

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -10,8 +10,9 @@ export const HomePage = () => {
       </Card>
 
       <Card type="red" title="Červená" img="../favicon.ico" imgPosition={"down"} paticka="Knedlenka.cz">
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem suscipit nemo id sequi vel quam molestias dolorum laboriosam. Quisquam, explicabo?</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam cumque quas nemo, veritatis provident laudantium, consectetur doloremque tenetur inventore atque soluta officiis incidunt voluptatibus placeat aperiam minus. Excepturi saepe aliquam libero molestiae, expedita, veritatis voluptatibus, voluptates maxime quidem itaque ipsam optio ex fuga fugiat modi sunt cupiditate repellat animi! Incidunt?</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem suscipit nemo id sequi vel quam molestias dolorum laboriosam. Quisquam, explicabo?</p>
+
       </Card>
 
       <Card type="green" title="Zelená" img="../favicon.ico" imgPosition={"middle"} paticka="Knedlenka.cz">
